be27053e491de3f67d1c6d850beac1033c8097fc
Gustavo Ushjima <lgu85@hotmail.com>
2021-07-09 10:48:35 -0300

    adding css

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+﻿const sass = require("node-sass")
+
+module.exports = function (grunt) {
+    "use strict"
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        watch: {
+            sass: {
+                files: "**/*.scss",
+                tasks: ["sass"],
+                options: {
+                    livereload: 35729,
+                },
+            },
+        },
+
+        // Sass
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: false, // Create source map
+                outputStyle: "uncompressed", // Minify output
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true, // Recursive
+                        cwd: "src/assets/themes", // The startup directory
+                        //src: ["**/*.scss"], // Source files
+                        src: ["**/sass-main.scss", "**/sass-login.scss"], // Source files
+                        dest: "src/assets/themes", // Destination
+                        ext: ".css", // File extension,
+                        rename: function (dest, src) {
+                            console.log(src)
+                            var file = src.replace(/SASS-/gi, "").toLowerCase()
+                            var args = file.split("/")
+                            var tenant = args.shift()
+                            var result = dest + "/" + tenant + "/" + args[0]
+                            return result
+                        },
+                    },
+                ],
+            },
+        },
+    })
+
+    // Load the plugin
+    grunt.loadNpmTasks("grunt-sass")
+    grunt.loadNpmTasks("grunt-contrib-watch")
+    // Default task(s).
+    grunt.registerTask("default", ["sass"])
+}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { useAuth } from "../../contexts/auth"
 
+import style from "../../assets/css/pages/_login.scss"
+
 export default function Login() {
     const { signIn } = useAuth()
 
@@ -10,7 +12,7 @@ export default function Login() {
 
     return (
         <div>
-            <h2>Login</h2>
+            <h2 className={style.bazinga}>Login</h2>
             <button onClick={handleSignIn}>SignIn</button>
         </div>
     )
